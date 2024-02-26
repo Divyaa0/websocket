a00151860e9108c0cd755744bf1bb66cb2559dae
unknown <divyaasoni0@gmail.com>
2024-02-26 23:44:38 +0530

    created websocket connection from client to server

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,37 +1,77 @@
 import React from "react";
-import chat_logo from "./style/images/logo3.png"
 import styles from "./style/home.module.css";
+import "./style/prime.css"
 import { Dropdown } from 'primereact/dropdown';
 import { useState } from "react";
+import io from 'socket.io-client';
 
 function Home() {
-  const [selectedCity, setSelectedCity] = useState(null);
-    const cities = [
-        { name: 'New York', code: 'NY' },
-        { name: 'Rome', code: 'RM' },
-        { name: 'London', code: 'LDN' },
-        { name: 'Istanbul', code: 'IST' },
-        { name: 'Paris', code: 'PRS' }
-    ];
+  const socket=io.connect('http://localhost:8000/');
  
+  const [selectedRoom, setSelectedRoom] = useState(null);
+  const [selectedName, setSelectedName] = useState(null);
+  const [Errors,setErrors]=useState('');
+  const [inputVal ,setInputVal]=useState({
+    name:"",
+    room:""
+  })
+
+    const Rooms = [
+        { name: 'Room1', code: '1' },
+        { name: 'Room2', code: '2' },
+           ];
+    // change Input
+    const inputEvent =(e)=>
+    {
+      console.log('input event')
+      setInputVal({...inputVal,[e.target.name]:e.target.value})
+    } 
+    // handle submit form
+    const handleSubmit =(e)=>
+    {
+      if( Validate_Input)
+      {
+        console.log("ssssssssssssss",inputVal)
+        e.preventDefault();
+        console.log("evnent submit triggered");
+        socket.emit('join_room',inputVal)
+      }
+    }
+  // Validate Input entered
+  const Validate_Input=(inputVal)=>
+  {
+    let bugs={};
+    if(!inputVal.name)
+    {
+      bugs.name="enter username !"
+    }
+    else if(inputVal.name.length>10)
+    {
+      bugs.name="username must be under 10 characters !"
+    }
+    if(!inputVal.room)
+    {
+      bugs.room="select a room"
+    }
+    console.log("bugs are : ",bugs);
+    setErrors(bugs);
+    return Object.keys(bugs).length===0;
+  }
   return (
     <div className={styles.container_home}>
       <div className={styles.login_form}>      
-      <form action="action_page.php" method="post">
+      <form onSubmit={handleSubmit}>
       <div className={styles.heading}>
         <h1>ChatVerse</h1>
       </div>
 
-  <div class="container_input">
-    <input type="text" placeholder="Enter Username" name="uname" required/>
+  <div className="container_input">
+    <input type="text" placeholder="Enter Username" name="name"  required onChange={inputEvent} value={inputVal.name}/>
      {/* dropdowns */}
      <div className="card flex justify-content-center">
-            <Dropdown value={selectedCity} onChange={(e) => setSelectedCity(e.value)} options={cities} optionLabel="name" 
-                className="w-full md:w-14rem" style={{backgroundColor:'rgb(255,255,255)', width:'100%'}} />
+            <Dropdown onChange={inputEvent} options={Rooms} optionLabel="name" name="room"
+                placeholder="Select Room" className={styles.dropdown} value={inputVal.room}  />
         </div>
-    
-    
-    
     <button type="submit">Join Room</button>
     
   </div>
--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 require('dotenv').config()
 const express=require('express')
 const http=require('http');
-const socketIO=require('socket.io')
-const port=process.env.PORT;
-
+const {Server}=require('socket.io')
+const cors=require('cors');
 const app=express();
+app.use(cors());
+
+// creating instance of server
 const ser̥ver=http.createServer(app)
 console.log("🚀 ~ ser̥ver:", ser̥ver)
+app.use(cors());
 
-app.listen(port,()=>console.log("server running on port",port));
+const io=new Server(ser̥ver,{cors:
+{origin:'http://localhost:3000',
+methods:['GET','POST'],
+transports:['websocket']}})
+
+// LISTEN EVENTS WHEN CLIENT CONNECTS VIA SOCKET.IO CLIENT
+io.on('connection',(socket)=>{
+console.log(`user connected with socket and id is ${socket.id}`)
+// LISTEN 'JOIN CHAT' EVENT
+socket.on('join_room', (data)=>{console.log("join chat entered by user " , data)})
+})
+ser̥ver.listen(8000,()=>{console.log("user connected")})
